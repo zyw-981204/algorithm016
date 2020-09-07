1e713674f144f0ffe0a4803e6dcf912a61060302
zyw-981204 <1364525128@qq.com>
2020-09-07 17:03:48 +0800

    完成装水最多的容器，时间复杂度O(n)

---
+++ Week_01/11.盛最多水的容器.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let l = 0,
+        r = height.length - 1,
+        maxArea = 0
+    while (l < r) {
+        let temp = (r - l) * Math.min(height[l], height[r])
+        maxArea = temp > maxArea ? temp : maxArea
+        height[l] > height[r] ? r-- : l++
+    }
+    return maxArea
+};
+// @lc code=end
+// @after-stub-for-debug-begin
+module.exports = maxArea;
+// @after-stub-for-debug-end
